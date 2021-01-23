ae8fe2bc1e09febb90cfc5d1cacf74629c73b220
Jan Gąsienica-Józkowy <jgjozkowy@gmail.com>
2021-01-23 20:42:01 +0100

    Init

---
+++ MANIFEST.in.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 23 20:17:19 2021
+
+@author: Andro
+"""
+
---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 23 20:08:47 2021
+
+@author: Andro
+"""
+
+__version__ = 1.0.0
